c07e97f649d4e85a8499052ad826a52f3c8b239f
aidanau8 <aidanausonbekova@gmail.com>
2025-11-07 15:45:23 -0500

    Initial Spring Boot project: InsightHub 🚀

---
+++ src/main/java/com/internship/InsightHub/Controller.java
@@ -0,0 +1,14 @@
+package com.internship.InsightHub;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from InsightHub 🚀";
+    }
+}
